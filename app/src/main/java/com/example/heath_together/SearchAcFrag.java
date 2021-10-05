6040c5175018373d6ae3c3a4fc7d48ad4d9206ef
yskeum <rmadbstn@naver.com>
2021-10-05 16:28:50 +0900

    2021-10-05

--- app/src/main/java/com/example/heath_together/SearchAcFrag.java
+++ app/src/main/java/com/example/heath_together/SearchAcFrag.java
@@ -5,17 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class Main3 extends Fragment {       //GroupScActivitiy
+//GroupScActivitiy
+public class SearchAcFrag extends Fragment {
 
     private String result;
     private View view;
@@ -27,21 +25,22 @@ public class Main3 extends Fragment {       //GroupScActivitiy
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.main3, container, false);
+        view = inflater.inflate(R.layout.main3_2, container, false);
 
-        moveButton = view.findViewById(R.id.changeAcButton);
-        recyclerView = view.findViewById(R.id.group_list);
+        moveButton = view.findViewById(R.id.changeGpButton);
+
+        recyclerView = view.findViewById(R.id.account_list);//have to find another list
 
 
         moveButton.setOnClickListener(new View.OnClickListener() {
 
-
             @Override
             public void onClick(View view) {
 
+
                 FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-                SearchAcFrag searchAcFrag = new SearchAcFrag();
-                transaction.replace(R.id.main_frame, searchAcFrag);
+                Main3 searchGpFrag= new Main3();
+                transaction.replace(R.id.main_frame, searchGpFrag);
                 transaction.commit();
 
             }
